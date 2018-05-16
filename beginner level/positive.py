fe9e41833812110476b673a0250e69adf906b385
balaguru12345 <36623700+balaguru12345@users.noreply.github.com>
2018-05-16 11:07:30 +0530

    Update positive.py

--- beginner level/positive.py
+++ beginner level/positive.py
@@ -1,7 +1,10 @@
 a=(int(input()))
-if (a>=1):
-    print "Positive"
-elif a==0:
-    print "Zero"
-else :
-    print "Negative"
+if(1<=a<=100000):
+    if (a>=1):
+        print "Positive"
+    elif a==0:
+        print "Zero"
+    else :
+        print "Negative"
+else:
+    print "Invalid Input"
