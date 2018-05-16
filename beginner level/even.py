ca897db68432224474ae92119281ff5813f44626
balaguru12345 <36623700+balaguru12345@users.noreply.github.com>
2018-05-16 11:21:42 +0530

    Create even.py

---
+++ beginner level/even.py
@@ -0,0 +1,12 @@
+
+n=(int(input()))
+if(1<=n<=100000):
+    if(n%2==0):
+        print "Even"
+    else:
+        print "Odd"
+elif(n==0):
+        print "Zero"
+else:
+        print "Invalid Input"
+    
